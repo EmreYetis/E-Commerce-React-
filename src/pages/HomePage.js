cbdd69bcc2ccfdf648f25715c7e34ca8d214c7db
Emre Yetis <emreyetis955@gmail.com>
2024-10-18 13:58:44 +0300

    update homepage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Grid } from '@mui/material';
+import { Typography } from '@mui/material';
 import ProductList from '../components/ProductList';
 import { useShop } from '../context/ShopContext';
 
